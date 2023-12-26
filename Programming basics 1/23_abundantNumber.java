4defcb673137489ab11f83b4d4a4bc3983731c7b
Sayantan Pandey <125211013+Sayantan018@users.noreply.github.com>
2023-12-26 21:13:01 +0530

    day3

---
+++ Programming basics 1/23_abundantNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class abundantNumber {
+    public static void main(String[] args){
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("enter the number : ");
+            int num=sc.nextInt();
+            int sum=0;
+
+            for(int i =1;i<num;i++){
+                if(num%i==0){
+                    sum=sum+i;
+                }
+            }
+            if(sum>num){
+                System.out.print(num+" is abundant number");
+            }
+            else System.out.print(num+" is not abundant number");
+        }
+    }
+}
